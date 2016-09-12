9a0a7333642b322c9ab4375be047ce60343eb11f
Eugene Burmako <xeno.by@gmail.com>
2016-09-12 17:48:04 +0100

    restores symbolAtPoint/typeAtPoint tests

--- macros/src/test/scala/DataMacroPcSpec.scala
+++ macros/src/test/scala/DataMacroPcSpec.scala
@@ -15,8 +15,18 @@ import org.ensime.pcplod._
 class DataMacroPcSpec extends FlatSpec {
 
   "@data" should "handle definitions of @data" in withPcPlod { pc =>
-    pc.loadScala("classes.scala")
+    val f = "classes.scala"
+    pc.loadScala(f)
     pc.messages shouldBe Nil
+
+    pc.symbolAtPoint(f, 'me) shouldBe Some("pctesting.Me")
+    pc.typeAtPoint(f, 'me) shouldBe Some("pctesting.Me")
+
+    pc.symbolAtPoint(f, 'myself) shouldBe Some("pctesting.Myself")
+    pc.typeAtPoint(f, 'myself) shouldBe Some("pctesting.Myself")
+
+    pc.symbolAtPoint(f, 'foo) shouldBe Some("pctesting.Myself.foo")
+    pc.typeAtPoint(f, 'foo) shouldBe Some("String")
   }
 
 }
